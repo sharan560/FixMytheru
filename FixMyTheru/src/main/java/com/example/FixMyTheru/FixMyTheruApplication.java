0cff4e64bcaf3d0cdd66b7982302abd6fb031f19
sharan560 <sharannagarajan06@gmail.com>
2025-07-17 11:18:44 +0530

    Project Intzialized

---
+++ FixMyTheru/src/main/java/com/example/FixMyTheru/FixMyTheruApplication.java
@@ -0,0 +1,13 @@
+package com.example.FixMyTheru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixMyTheruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixMyTheruApplication.class, args);
+	}
+
+}
---
+++ FixMyTheru/src/test/java/com/example/FixMyTheru/FixMyTheruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FixMyTheru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixMyTheruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
